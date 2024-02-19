db2731b2bb6e8220d05cb31d46a2c3029062e7af
nahid <nahidrabbi045@gmail.com>
2024-02-19 06:53:33 -0700

    ticket buy

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+//Disabled Buttons
+document.getElementById("apply-coupon-btn").setAttribute("disabled", true);
+document.getElementById("next-btn").setAttribute("disabled", true);
+//Disabled Buttons
+// Buy Ticket Operation
+document
+  .getElementById("btn-buy-tickets")
+  .addEventListener("click", function () {
+    document.querySelector("header").classList.add("hidden");
+    document.querySelector("footer").classList.add("hidden");
+    document.getElementById("coupon").classList.add("hidden");
+  });
+// Buy Ticket Operation
