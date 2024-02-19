f0dc9690f5af018ff7909e0da7d1767a1ecf84a0
nahid <nahidrabbi045@gmail.com>
2024-02-19 06:55:44 -0700

    button problem solve

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,12 @@
-//Disabled Buttons
+//Disabled Buttons------------------------------------------
 document.getElementById("apply-coupon-btn").setAttribute("disabled", true);
 document.getElementById("next-btn").setAttribute("disabled", true);
-//Disabled Buttons
-// Buy Ticket Operation
-document
-  .getElementById("btn-buy-tickets")
-  .addEventListener("click", function () {
+
+// Buy Ticket Operation---------------------------------------
+
+document.getElementById("btn-buy-tickets")
+    .addEventListener("click", function () {
     document.querySelector("header").classList.add("hidden");
     document.querySelector("footer").classList.add("hidden");
     document.getElementById("coupon").classList.add("hidden");
   });
-// Buy Ticket Operation
